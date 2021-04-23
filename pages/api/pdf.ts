d243fcfd0c26e847d733aa2eb33f404715a0a647
tgeorgiadis <tgeorg94@gmail.com>
2021-04-23 13:00:46 +1000

    Test playwright deployment on Vercel

---
+++ pages/api/pdf.ts
@@ -0,0 +1,34 @@
+import {NextApiRequest, NextApiResponse} from 'next'
+import playwright from 'playwright-core';
+import chromium from 'chrome-aws-lambda';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    await chromium.font("https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf")
+    const browser = await playwright.chromium.launch({
+      args: chromium.args,
+      executablePath: await chromium.executablePath || '/usr/bin/google-chrome',
+      headless: chromium.headless,
+    });
+
+    const page = await browser.newPage({
+      viewport: {
+        width: 1200,
+        height: 630
+      }
+    });
+    //const url = getAbsoluteURL(req.query["path"] as string || "")
+    await page.goto('https://github.com/vercel/vercel/discussions/4903', {
+      timeout: 15 * 1000
+    })
+    const data = await page.screenshot({
+      type: "png"
+    })
+    await browser.close()
+    res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate")
+    res.setHeader('Content-Type', 'image/png')
+    res.end(data)
+  } catch (err) {
+    console.error(err)
+  }
+}
