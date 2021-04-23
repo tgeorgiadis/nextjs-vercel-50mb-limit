36ce81e5e6cef95933d9c181c46f6c1fe08085c8
tgeorgiadis <tgeorg94@gmail.com>
2021-04-23 13:51:31 +1000

    Add playwright for local usage. Use playwright for local and playwright-core for prod

--- pages/api/pdf.ts
+++ pages/api/pdf.ts
@@ -2,33 +2,49 @@ import {NextApiRequest, NextApiResponse} from 'next'
 import playwright from 'playwright-core';
 import chromium from 'chrome-aws-lambda';
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    await chromium.font("https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf")
-    const browser = await playwright.chromium.launch({
-      args: chromium.args,
-      executablePath: await chromium.executablePath || '/usr/bin/google-chrome',
-      headless: chromium.headless,
-    });
+async function getBrowserInstance() {
+  const executablePath = await chromium.executablePath
 
-    const page = await browser.newPage({
-      viewport: {
-        width: 1200,
-        height: 630
-      }
-    });
-    //const url = getAbsoluteURL(req.query["path"] as string || "")
-    await page.goto('https://github.com/vercel/vercel/discussions/4903', {
-      timeout: 15 * 1000
+  // running locally
+  if (!executablePath) {
+    const playwright = require('playwright')
+    return playwright.chromium.launch({
+      args: chromium.args,
+      headless: true,
+      ignoreHTTPSErrors: true
     })
-    const data = await page.screenshot({
-      type: "png"
-    })
-    await browser.close()
-    res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate")
-    res.setHeader('Content-Type', 'image/png')
-    res.end(data)
-  } catch (err) {
-    console.error(err)
   }
+
+  return playwright.chromium.launch({
+    args: chromium.args,
+    executablePath: executablePath,
+    headless: chromium.headless,
+  })
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+try {
+  await chromium.font("https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf")
+  const browser = await getBrowserInstance()
+
+  const page = await browser.newPage({
+    viewport: {
+      width: 1200,
+      height: 630
+    }
+  });
+  //const url = getAbsoluteURL(req.query["path"] as string || "")
+  await page.goto('https://github.com/vercel/vercel/discussions/4903', {
+    timeout: 15 * 1000
+  })
+  const data = await page.screenshot({
+    type: "png"
+  })
+  await browser.close()
+  res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate")
+  res.setHeader('Content-Type', 'image/png')
+  res.end(data)
+} catch (err) {
+  console.error(err)
+}
 }
