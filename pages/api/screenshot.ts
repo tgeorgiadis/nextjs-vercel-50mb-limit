a84ccefeaa047bc931bb99df54a04dadb46658de
tgeorgiadis <tgeorg94@gmail.com>
2021-04-24 12:48:55 +1000

    Fix Puppeteer not working on deployment due to relic playwright options. Use webpack 5

--- pages/api/screenshot.ts
+++ pages/api/screenshot.ts
@@ -1,10 +1,33 @@
 import {NextApiRequest, NextApiResponse} from 'next'
 import chromium from 'chrome-aws-lambda'
 
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  let browser = null
+
+  try {
+    browser = await getBrowserInstance()
+    const page = await browser.newPage()
+    await page.goto('https://github.com/vercel/vercel/issues/4739')
+    const data = await page.screenshot()
+    res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate")
+    res.setHeader('Content-Type', 'image/png')
+    res.end(data)
+  } catch (error) {
+    console.error(error)
+    return res.status(400).json({
+      error: error || 'Could not take screenshot'
+    })
+  } finally {
+    if (browser !== null) {
+      await browser.close()
+    }
+  }
+}
+
 const getBrowserInstance = async () => {
   const executablePath = await chromium.executablePath
 
-  // running locally
+  //running locally
   if (!executablePath) {
     const puppeteer = require('puppeteer')
     return puppeteer.launch({
@@ -16,44 +39,9 @@ const getBrowserInstance = async () => {
 
   return chromium.puppeteer.launch({
     args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
     executablePath: executablePath,
     headless: chromium.headless,
     ignoreHTTPSErrors: true
   })
-}
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  let browser = null
-
-  try {
-    browser = await getBrowserInstance() as any
-
-    const page = await browser.newPage({
-      viewport: {
-        width: 1200,
-        height: 630
-      }
-    })
-
-    await page.goto('https://github.com/vercel/vercel/issues/4739')
-
-    const data = await page.screenshot({
-      type: "png"
-    })
-
-    res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate")
-    res.setHeader('Content-Type', 'image/png')
-    res.end(data)
-
-  } catch (error) {
-    console.error(error)
-    return res.status(400).json({
-      error: error || 'Could not take screenshot'
-    })
-  } finally {
-    if (browser !== null) {
-      await browser.close()
-    }
-  }
-
 }
